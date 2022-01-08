33824ed6d0fd21e41e370bded48b701b8d9f9b13
Vaibhav Garg <vaibhavgarg90@gmail.com>
2022-01-09 01:27:43 +0530

    added solution for problem 1137

---
+++ solutions/leetcode/editor/en/[1137]N-th Tribonacci Number.py
@@ -0,0 +1,46 @@
+# The Tribonacci sequence Tn is defined as follows: 
+# 
+#  T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0. 
+# 
+#  Given n, return the value of Tn. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: n = 4
+# Output: 4
+# Explanation:
+# T_3 = 0 + 1 + 1 = 2
+# T_4 = 1 + 1 + 2 = 4
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: n = 25
+# Output: 1389537
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  0 <= n <= 37 
+#  The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 -
+#  1. 
+#  Related Topics Math Dynamic Programming Memoization 👍 1364 👎 86
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n in (1, 2):
+            return 1
+        a, b, c = 0, 1, 1
+        for _ in range(2, n):
+            a, b, c = b, c, a + b + c
+        return c
+# leetcode submit region end(Prohibit modification and deletion)
