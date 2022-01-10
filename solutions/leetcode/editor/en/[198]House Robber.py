f4a5ac73e778a052278d76d90a372223c052cb02
Vaibhav Garg <vaibhavgarg90@gmail.com>
2022-01-10 16:04:04 +0530

    added solution for problem 198

---
+++ solutions/leetcode/editor/en/[198]House Robber.py
@@ -0,0 +1,56 @@
+# You are a professional robber planning to rob houses along a street. Each 
+# house has a certain amount of money stashed, the only constraint stopping you from 
+# robbing each of them is that adjacent houses have security systems connected and 
+# it will automatically contact the police if two adjacent houses were broken 
+# into on the same night. 
+# 
+#  Given an integer array nums representing the amount of money of each house, 
+# return the maximum amount of money you can rob tonight without alerting the 
+# police. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [1,2,3,1]
+# Output: 4
+# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+# Total amount you can rob = 1 + 3 = 4.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [2,7,9,3,1]
+# Output: 12
+# Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 
+# (money = 1).
+# Total amount you can rob = 2 + 9 + 1 = 12.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= nums.length <= 100 
+#  0 <= nums[i] <= 400 
+#  
+#  Related Topics Array Dynamic Programming 👍 10335 👎 230
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from functools import cache
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        @cache
+        def rec(i):
+            if i >= n:
+                return 0
+            return max(nums[i] + rec(i + 2), nums[i] + rec(i + 3), rec(i + 1))
+
+        return rec(0)
+# leetcode submit region end(Prohibit modification and deletion)
