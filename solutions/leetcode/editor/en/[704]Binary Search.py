344f8c9e9bac8b34c826d77b91c352a4932d9fcc
Vaibhav Garg <vaibhavgarg90@gmail.com>
2022-01-16 14:25:43 +0530

    added solution for problem 704

---
+++ solutions/leetcode/editor/en/[704]Binary Search.py
@@ -0,0 +1,50 @@
+# Given an array of integers nums which is sorted in ascending order, and an 
+# integer target, write a function to search target in nums. If target exists, then 
+# return its index. Otherwise, return -1. 
+# 
+#  You must write an algorithm with O(log n) runtime complexity. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in nums so return -1
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= nums.length <= 10⁴ 
+#  -10⁴ < nums[i], target < 10⁴ 
+#  All the integers in nums are unique. 
+#  nums is sorted in ascending order. 
+#  
+#  Related Topics Array Binary Search 👍 3135 👎 86
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l: int = 0
+        h: int = len(nums) - 1
+        while l <= h:
+            mid = (l + h) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                h = mid - 1
+        return -1
+# leetcode submit region end(Prohibit modification and deletion)
