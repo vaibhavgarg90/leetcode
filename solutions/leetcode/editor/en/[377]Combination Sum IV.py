6883aeada355df63524f4da715e2c9a7d49c82a8
Vaibhav Garg <vaibhavgarg90@gmail.com>
2022-01-18 19:37:35 +0530

    added solution for problem 377

---
+++ solutions/leetcode/editor/en/[377]Combination Sum IV.py
@@ -0,0 +1,68 @@
+# Given an array of distinct integers nums and a target integer target, return 
+# the number of possible combinations that add up to target. 
+# 
+#  The test cases are generated so that the answer can fit in a 32-bit integer. 
+# 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [1,2,3], target = 4
+# Output: 7
+# Explanation:
+# The possible combination ways are:
+# (1, 1, 1, 1)
+# (1, 1, 2)
+# (1, 2, 1)
+# (1, 3)
+# (2, 1, 1)
+# (2, 2)
+# (3, 1)
+# Note that different sequences are counted as different combinations.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [9], target = 3
+# Output: 0
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= nums.length <= 200 
+#  1 <= nums[i] <= 1000 
+#  All the elements of nums are unique. 
+#  1 <= target <= 1000 
+#  
+# 
+#  
+#  Follow up: What if negative numbers are allowed in the given array? How does 
+# it change the problem? What limitation we need to add to the question to allow 
+# negative numbers? 
+#  Related Topics Array Dynamic Programming 👍 3025 👎 345
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from functools import cache
+
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        nums.sort()
+
+        @cache
+        def rec(target):
+            res = 0
+            for num in nums:
+                if num == target:
+                    res += 1
+                elif num < target:
+                    res += rec(target - num)
+            return res
+
+        return rec(target)
+# leetcode submit region end(Prohibit modification and deletion)
