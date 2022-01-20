f2cced2807ae0d8300542baf11f2690b9b169d31
Vaibhav Garg <vaibhavgarg90@gmail.com>
2022-01-20 21:30:02 +0530

    added solution for problem 875

---
+++ solutions/leetcode/editor/en/[875]Koko Eating Bananas.py
@@ -0,0 +1,70 @@
+# Koko loves to eat bananas. There are n piles of bananas, the iᵗʰ pile has 
+# piles[i] bananas. The guards have gone and will come back in h hours. 
+# 
+#  Koko can decide her bananas-per-hour eating speed of k. Each hour, she 
+# chooses some pile of bananas and eats k bananas from that pile. If the pile has less 
+# than k bananas, she eats all of them instead and will not eat any more bananas 
+# during this hour. 
+# 
+#  Koko likes to eat slowly but still wants to finish eating all the bananas 
+# before the guards return. 
+# 
+#  Return the minimum integer k such that she can eat all the bananas within h 
+# hours. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: piles = [3,6,7,11], h = 8
+# Output: 4
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: piles = [30,11,23,4,20], h = 5
+# Output: 30
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: piles = [30,11,23,4,20], h = 6
+# Output: 23
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= piles.length <= 10⁴ 
+#  piles.length <= h <= 10⁹ 
+#  1 <= piles[i] <= 10⁹ 
+#  
+#  Related Topics Array Binary Search 👍 3030 👎 145
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from math import ceil
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        def is_feasible(k: int) -> bool:
+            _count = 0
+            for pile in piles:
+                _count += ceil(pile/k)
+            return _count <= h
+
+        start = 1
+        end = max(piles)
+        while start <= end:
+            mid = (start + end) // 2
+            if is_feasible(mid):
+                end = mid - 1
+            else:
+                start = mid + 1
+        return start
+# leetcode submit region end(Prohibit modification and deletion)
