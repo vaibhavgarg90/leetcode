77c9252c8a5902d87d11c0e9290c1c844f490dc3
Vaibhav Garg <vaibhavgarg90@gmail.com>
2022-02-24 20:53:44 +0530

    added solution for problem 155

---
+++ solutions/leetcode/editor/en/[155]Min Stack.py
@@ -0,0 +1,77 @@
+# Design a stack that supports push, pop, top, and retrieving the minimum 
+# element in constant time. 
+# 
+#  Implement the MinStack class: 
+# 
+#  
+#  MinStack() initializes the stack object. 
+#  void push(int val) pushes the element val onto the stack. 
+#  void pop() removes the element on the top of the stack. 
+#  int top() gets the top element of the stack. 
+#  int getMin() retrieves the minimum element in the stack. 
+#  
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input
+# ["MinStack","push","push","push","getMin","pop","top","getMin"]
+# [[],[-2],[0],[-3],[],[],[],[]]
+# 
+# Output
+# [null,null,null,null,-3,null,0,-2]
+# 
+# Explanation
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin(); // return -3
+# minStack.pop();
+# minStack.top();    // return 0
+# minStack.getMin(); // return -2
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  -2³¹ <= val <= 2³¹ - 1 
+#  Methods pop, top and getMin operations will always be called on non-empty 
+# stacks. 
+#  At most 3 * 10⁴ calls will be made to push, pop, top, and getMin. 
+#  
+#  Related Topics Stack Design 👍 7418 👎 589
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# leetcode submit region end(Prohibit modification and deletion)
