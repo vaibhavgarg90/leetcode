225bab43c8f911d9bb6c0b83043a18faca2d989b
Vaibhav Garg <vaibhavgarg90@gmail.com>
2023-04-14 13:11:39 +0530

    fix the `leetcode.filePath` setting of VSCode leetcode extension
    - the setting now matches the file name format of the IntelliJ IDEA LeetCode Edior Plugin

---
+++ solutions/leetcode/editor/en/[182]Duplicate Emails.sql
@@ -0,0 +1,13 @@
+--
+-- @lc app=leetcode id=182 lang=mysql
+--
+-- [182] Duplicate Emails
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+SELECT email FROM Person GROUP BY email HAVING COUNT(email) > 1;
+
+-- @lc code=end
+
